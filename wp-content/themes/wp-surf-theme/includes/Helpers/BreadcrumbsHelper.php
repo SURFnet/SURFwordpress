03f1a3c622b173ed987423ba15e39362eabb8a11
Van Ons (bot) <info@van-ons.nl>
2026-06-23 14:27:52 +0000

    fix: Resolve PHP Warning:  Undefined property WP_Post_Type

--- wp-content/themes/wp-surf-theme/includes/Helpers/BreadcrumbsHelper.php
+++ wp-content/themes/wp-surf-theme/includes/Helpers/BreadcrumbsHelper.php
@@ -9,33 +9,41 @@ namespace SURF\Helpers;
 class BreadcrumbsHelper
 {
 
-	/**
-	 * @return bool
-	 */
-	public static function shouldShow(): bool
-	{
-		if ( is_front_page() || !function_exists( 'yoast_breadcrumb' ) ) {
-			return false;
-		}
+    /**
+     * @return bool
+     */
+    public static function shouldShow(): bool
+    {
+        if (is_front_page() || !function_exists('yoast_breadcrumb')) {
+            return false;
+        }
 
-		$hide_crumbs = get_option( 'options_deactivate_breadcrumbs' );
-		if ( !$hide_crumbs || !is_array( $hide_crumbs ) ) {
-			$hide_crumbs = [];
-		}
+        $hide_crumbs = get_option('options_deactivate_breadcrumbs');
+        if (!$hide_crumbs || !is_array($hide_crumbs)) {
+            $hide_crumbs = [];
+        }
 
-		$should_hide = in_array( get_post_type(), $hide_crumbs );
-		if ( $should_hide ) {
-			return false;
-		}
+        $should_hide = in_array(get_post_type(), $hide_crumbs);
+        if ($should_hide) {
+            return false;
+        }
 
-		$queried_obj = get_queried_object();
-		$taxonomy    = $queried_obj?->taxonomy;
-		$tax_obj     = get_taxonomy( $taxonomy );
-		if ( array_intersect( $tax_obj?->object_type ?? [], $hide_crumbs ) ) {
-			return false;
-		}
+        $queried_obj = get_queried_object();
+        if (!property_exists($queried_obj, 'taxonomy')) {
+            return true;
+        }
 
-		return true;
-	}
+        $taxonomy = $queried_obj->taxonomy;
+        $tax_obj  = get_taxonomy($taxonomy);
+        if (empty($tax_obj)) {
+            return true;
+        }
+
+        if (array_intersect($tax_obj->object_type ?? [], $hide_crumbs)) {
+            return false;
+        }
+
+        return true;
+    }
 
 }
